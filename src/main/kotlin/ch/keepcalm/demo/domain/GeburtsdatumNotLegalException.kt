ec513e503e7f8bee20080680829aaa35d54ff71b
Marcel Widmer <marzelwidmer@gmail.com>
2022-10-02 13:06:52 +0200

    wip

--- src/main/kotlin/ch/keepcalm/demo/domain/GeburtsdatumNotLegalException.kt
+++
@@ -1,6 +0,0 @@
-package ch.keepcalm.demo.domain
-
-import ch.keepcalm.demo.infrastructure.exception.BusinessException
-
-class GeburtsdatumNotLegalException(birthday: Geburtsdatum) : BusinessException("Not legal age [${birthday.value}]")
-
--- src/main/kotlin/ch/keepcalm/demo/infrastructure/exception/BusinessException.kt
+++
@@ -1,3 +0,0 @@
-package ch.keepcalm.demo.infrastructure.exception
-
-open class BusinessException(message: String, cause: Throwable? = null) : RuntimeException(message, cause)
